5d5f15337bc8ee1842a8459f08103d1a3db0a575
thayanealves <thayanemarciano4@gmail.com>
2024-09-11 14:53:40 -0300

    conversão em doláres

---
+++ atvd08/atvd08.py
@@ -0,0 +1,8 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+reais= int(input("digite o valor em reais"))
+taxa_cambio= 5.60
+
+conversao= reais/taxa_cambio
+
+print (f"o valor em dolares é igual é{conversao}")
+
